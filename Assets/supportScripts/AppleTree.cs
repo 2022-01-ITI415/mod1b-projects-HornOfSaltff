fe8c407a8d97f182b56fb50d176e80b4f696fe2b
ERIC YANG <ecy14@scarletmail.rutgers.edu>
2022-02-03 11:40:42 -0500

    updated the script for appletree

--- Assets/supportScripts/AppleTree.cs
+++ Assets/supportScripts/AppleTree.cs
@@ -11,6 +11,7 @@ public class AppleTree : MonoBehaviour
     public float chanceToChangeDirections = 0.1f;
     public float secondsBetweenAppleDrops = 1f;
     // Start is called before the first frame update
+    
     void Start()
     {
         
@@ -19,6 +20,30 @@ public class AppleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Basic Movement
+        Vector3 pos = transform.position;
+
+        pos.x += speed * Time.deltaTime;
+
+        transform.position = pos;
+
+        // Changing Direction
+
+        if ( post. < -leftAndRightEdge ) {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if ( pos.x > leftAndRightEdge ) {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+        else if ( Random.value < chanceToChangeDirections ) {
+            speed *= -1; // Change direction
+        }
+    }
+
+    void FixedUpdate() {
+        //Changing Direction Randomly is now t
+        if ( Random.value < chanceToChangeDirections) {
+            speed *= -1; // Change direction
+        }
     }
 }
