fdec2a19168fe9422b2fd2bd56004046ba7977b5
ERIC YANG <ecy14@scarletmail.rutgers.edu>
2022-02-03 16:41:25 -0500

    ApplePicker script + Apple + AppleTree

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -12,7 +12,6 @@ public class AppleTree : MonoBehaviour {
 
     public GameObject   applePrefab;
 
-
                    
     // Speed at which the AppleTree moves
 
@@ -41,9 +40,18 @@ public class AppleTree : MonoBehaviour {
     void Start () {
 
         // Dropping apples every second
-
+        Invoke( "DropApple", 2f );
     }
 
+    void DropApple() {                                                  // b
+
+        GameObject apple = Instantiate<GameObject>( applePrefab );      // c
+
+        apple.transform.position = transform.position;                  // d
+
+        Invoke( "DropApple", secondsBetweenAppleDrops );                // e
+
+    }
 
                    
     void Update () {
