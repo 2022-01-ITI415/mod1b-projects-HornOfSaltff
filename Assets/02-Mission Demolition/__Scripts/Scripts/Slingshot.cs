df9d51d6e4e123c54363a49bfe8197afd9480c5c
ERIC YANG <ecy14@scarletmail.rutgers.edu>
2022-02-10 17:05:22 -0500

    W3 Adding a Projectile Trail reset cam still not working

--- Assets/02-Mission Demolition/__Scripts/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/__Scripts/Scripts/Slingshot.cs
@@ -7,7 +7,7 @@ using System.Collections;
 public class Slingshot : MonoBehaviour {
 
     // fields set in the Unity Inspector pane
-
+    static private Slingshot S;  
     [Header("Set in Inspector")]                                            // a
 
     public GameObject prefabProjectile;
@@ -29,7 +29,20 @@ public class Slingshot : MonoBehaviour {
 
     private Rigidbody projectileRigidbody; 
 
+    static public Vector3 LAUNCH_POS {                                        // b
+
+        get {
+
+            if (S == null ) return Vector3.zero;
+
+            return S.launchPos;
+
+        }
+
+    }
+
     void Awake() {
+        S = this;
 
         Transform launchPointTrans = transform.Find("LaunchPoint");              // a
 
