a83029b89990c93731a627d50d612649aefc737b
ERIC YANG <ecy14@scarletmail.rutgers.edu>
2022-02-10 16:49:16 -0500

    W3 Returning for Another Shot, but its not returning

--- Assets/02-Mission Demolition/__Scripts/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/__Scripts/Scripts/FollowCam.cs
@@ -39,7 +39,41 @@ public class FollowCam : MonoBehaviour {
 
         // Get the position of the poi
 
-        Vector3 destination = POI.transform.position;
+        //Vector3 destination = POI.transform.position;
+
+        Vector3 destination;
+        if (POI == null ) {
+
+            destination = Vector3.zero;
+
+        }
+        else {
+
+            // Get the position of the poi
+
+            destination = POI.transform.position;
+
+            // If poi is a Projectile, check to see if it's at rest
+
+            if (POI.tag == "Projectile" ) {
+
+                // if it is sleeping (that is, not moving)
+
+                if ( POI.GetComponent<Rigidbody>().IsSleeping() ) {
+
+                    // return to default view
+
+                    POI = null ;
+
+                    // in the next update
+
+                    return ;
+
+                }
+
+            }
+
+        }
         // Limit the X & Y to minimum values
 
         destination.x = Mathf.Max( minXY.x, destination.x );
