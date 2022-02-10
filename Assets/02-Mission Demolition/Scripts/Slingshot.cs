d650a554a7d00eb5e9c1ea5b27da3c4f56a9a2fb
ERIC YANG <ecy14@scarletmail.rutgers.edu>
2022-02-10 13:16:03 -0500

    W3 follow cam works

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -134,6 +134,8 @@ public class Slingshot : MonoBehaviour {
 
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
 
+            FollowCam.POI = projectile;
+
             projectile = null;
 
         }
