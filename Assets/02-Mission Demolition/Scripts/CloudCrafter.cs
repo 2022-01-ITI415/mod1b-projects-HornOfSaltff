79989abcc624d4594b3189598635ccc6037cccbb
ERIC YANG <ecy14@scarletmail.rutgers.edu>
2022-02-10 16:07:23 -0500

    made cloudcrafter
    clouds no longer spawn

--- Assets/02-Mission Demolition/Scripts/CloudCrafter.cs
+++ Assets/02-Mission Demolition/Scripts/CloudCrafter.cs
@@ -1,86 +1,129 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CloudCrafter : MonoBehaviour
-{
-	[Header("Set in Inspector")]
-	public int 	numClouds = 40; // the number of clouds to make
-	public GameObject 	cloudPrefab; // the prefab for the clouds
-	public Vector3 	cloudPosMin = new Vector3(-50, -5, 10);
-	public Vector3 	cloudPosMax = new Vector3(150, 100, 10);
-	public float 	cloudScaleMin = 1; // min scale for the clouds
-	public float 	cloudScaleMax = 3; // max scale for the clouds
-	public float 	cloudSpeedMult = 0.5f; // adjusts speed of clouds
+using System.Collections;
 
-	private GameObject[] 	cloudInstances;
 
-	void Awake() {
-		// make an array large enough to hold all the Cloud instances
-		cloudInstances = new GameObject[numClouds];
 
-		// find the CloudAnchor parent GameObject
-		GameObject anchor = GameObject.Find("CloudAnchor");
+public class CloudCrafter : MonoBehaviour {
 
-		// iterate through and make clouds
-		GameObject cloud;
-		for (int i = 0; i < numClouds; i++) {
-			// make an instance of cloudPrefab
-			cloud = Instantiate<GameObject>(cloudPrefab);
+    [Header("Set in Inspector")]
 
-			// position of cloud
-			Vector3 cPos = Vector3.zero;
-			cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
-			cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+    public int            numClouds = 40;         // The # of clouds to make
 
-			// scale cloud
-			float scaleU = Random.value;
-			float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
+    public GameObject     cloudPrefab;            // The prefab for the clouds
 
-			// smaller clouds (with smaller scaleU) should be near the ground
-			cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
+    public Vector3        cloudPosMin = new Vector3(-50,-5,10);
 
-			// smaller clouds should be farther away
-			cPos.z = 100 -90*scaleU;
+    public Vector3        cloudPosMax = new Vector3(150,100,10);
 
-			// apply these transforms to the cloud 
-			cloud.transform.position = cPos;
-			cloud.transform.localScale = Vector3.one * scaleVal;
+    public float          cloudScaleMin = 1;     // Min scale of each cloud
 
-			// make a cloud a child of the anchor
-			cloud.transform.SetParent(anchor.transform);
+    public float          cloudScaleMax = 3;     // Max scale of each cloud
 
-			// add the cloud to cloudInstances
-			cloudInstances[i] = cloud;
-		}
-	}
+    public float          cloudSpeedMult = 0.5f; // Adjusts speed of clouds
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // iterate each over cloud that was created 
-        foreach (GameObject cloud in cloudInstances) {
-        	// get the cloud scale and position
-        	float scaleVal = cloud.transform.localScale.x;
-        	Vector3 cPos = cloud.transform.position;
 
-        	// move larger clouds faster
-        	cPos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
 
-        	// if a cloud has moved too far on the left
-        	if (cPos.x <= cloudPosMin.x) {
-        		// move it to the far right 
-        		cPos.x = cloudPosMax.x;
-        	}
+    private GameObject[]  cloudInstances;
 
-        	// apply the new position to cloud
-        	cloud.transform.position = cPos;
+
+
+    void Awake() {
+
+        // Make an array large enough to hold all the Cloud_ instances
+
+        cloudInstances = new GameObject[numClouds];
+
+        // Find the CloudAnchor parent GameObject
+
+        GameObject anchor = GameObject.Find("CloudAnchor");
+
+        // Iterate through and make Cloud_s
+
+        GameObject cloud;
+
+        for (int i=0; i<numClouds; i++) {
+
+            // Make an instance of cloudPrefab
+
+            cloud = Instantiate<GameObject>( cloudPrefab );
+
+            // Position cloud
+
+            Vector3 cPos = Vector3.zero;
+
+            cPos.x = Random.Range( cloudPosMin.x, cloudPosMax.x );
+
+            cPos.y = Random.Range( cloudPosMin.y, cloudPosMax.y );
+
+            // Scale cloud
+
+            float scaleU = Random.value;
+
+            float scaleVal = Mathf.Lerp( cloudScaleMin, cloudScaleMax, scaleU );
+
+            // Smaller clouds (with smaller scaleU) should be nearer the ground
+
+            cPos.y = Mathf.Lerp( cloudPosMin.y, cPos.y, scaleU );
+
+            // Smaller clouds should be further away
+
+            cPos.z = 100 - 90*scaleU;
+
+            // Apply these transforms to the cloud
+
+            cloud.transform.position = cPos;
+
+            cloud.transform.localScale = Vector3.one * scaleVal;
+
+            // Make cloud a child of the anchor
+
+            cloud.transform.SetParent( anchor.transform );
+
+            // Add the cloud to cloudInstances
+
+            cloudInstances[i] = cloud;
+
         }
+
     }
+
+
+
+    void Update() {
+
+        // Iterate over each cloud that was created
+
+        foreach (GameObject cloud in cloudInstances) {
+
+            // Get the cloud scale and position
+
+            float scaleVal = cloud.transform.localScale.x;
+
+            Vector3 cPos = cloud.transform.position;
+
+            // Move larger clouds faster
+
+            cPos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
+
+
+            // If a cloud has moved too far to the left...
+
+            if (cPos.x <= cloudPosMin.x) {
+
+                // Move it to the far right
+
+                cPos.x = cloudPosMax.x;
+
+            }
+
+            // Apply the new position to cloud
+
+            cloud.transform.position = cPos;
+
+        }
+
+   }
+
 }
--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -9,10 +9,14 @@ public class FollowCam : MonoBehaviour {
     static public GameObject POI; // The static point of interest                // a
 
 
+    [Header("Set in Inspector")]
+
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
 
     [Header("Set Dynamically")]
 
-    public float             camZ; // The desired Z pos of the camera
+    public float camZ; // The desired Z pos of the camera
 
 
 
@@ -36,6 +40,15 @@ public class FollowCam : MonoBehaviour {
         // Get the position of the poi
 
         Vector3 destination = POI.transform.position;
+        // Limit the X & Y to minimum values
+
+        destination.x = Mathf.Max( minXY.x, destination.x );
+
+        destination.y = Mathf.Max( minXY.y, destination.y );
+
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        
 
         // Force destination.z to be camZ to keep the camera far enough away
 
@@ -44,6 +57,9 @@ public class FollowCam : MonoBehaviour {
         // Set the camera to the destination
 
         transform.position = destination;
+        // Set the orthographicSize of the Camera to keep Ground in view
+
+        Camera.main.orthographicSize = destination.y + 10;
 
     }
 
